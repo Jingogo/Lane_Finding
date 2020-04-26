ceb9d18d7d7b668235f3c915e4acdfb69b45f3c4
Jing <jinglu.kong@gmail.com>
2020-04-26 20:12:25 +0200

    update

--- src/example.py
+++ src/example.py
@@ -3,31 +3,23 @@ import cv2
 import matplotlib.pyplot as plt
 
 def perspective_transform(img, src, dst):
-
-    # Compute and apply perpective transform
     img_size = (img.shape[1], img.shape[0])
     M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
-
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST) 
     return warped
 
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Apply x or y gradient with the OpenCV Sobel() function
-    # and take the absolute value
+    
     if orient == 'x':
         abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize =  
                     sobel_kernel))
     if orient == 'y':
         abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = 
                     sobel_kernel))
-    # Rescale back to 8 bit integer
     scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-    # Create a copy and apply the threshold
     binary_output = np.zeros_like(scaled_sobel)
-    # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
     binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-    # Return the result
     return binary_output
 
 def grad_thresh(img, sobel_kernel=3, thresh=(0, 255)):   
