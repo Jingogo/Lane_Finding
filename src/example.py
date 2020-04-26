8a2ee3c0d5d5dc8f54835ec8623f2466ba835424
Jing <jinglu.kong@gmail.com>
2020-04-26 12:16:07 +0200

    update

--- src/example.py
+++ src/example.py
@@ -172,4 +172,33 @@ def fit_polynomial(binary_warped):
 
     return out_img
 
+def fit_polynomial_actual(binary_warped, ym_per_pix, xm_per_pix):
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+   
+    return left_fit_cr, right_fit_cr
+
+    
+def measure_curvature_real(binary_warped, ym_per_pix, xm_per_pix):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    
+    # Start by generating our fake example data
+    # Make sure to feed in your real data instead in your project!
+    left_fit_cr, right_fit_cr = fit_polynomial_actual(binary_warped, ym_per_pix, xm_per_pix)
+    
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = (binary_warped.shape[0]-1)*ym_per_pix
+    
+    ##### TO-DO: Implement the calculation of R_curve (radius of curvature) #####
+    left_curverad = ((1+ (2*left_fit_cr[0]*y_eval+left_fit_cr[1])**2)**1.5) / (2*abs(left_fit_cr[0]))
+    right_curverad = ((1+ (2*right_fit_cr[0]*y_eval+right_fit_cr[1])**2)**1.5) / (2*abs(right_fit_cr[0]))
+    
+    return left_curverad, right_curverad
 
